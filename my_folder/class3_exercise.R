f542d8aa7de5a80d51e43bd059d80c3821c122e0
Karansheraz <Karansheraz.powar@gmail.com>
2018-03-05 10:43:56 -0800

    Add files via upload

---
+++ my_folder/class3_exercise.R
@@ -0,0 +1,16 @@
+library(knitr)
+library(tidyverse)
+
+set.seed(87)
+dat <- tibble(x = c(rnorm(100), rnorm(100)+5)-3,
+              y = sin(x^2/5)/x + rnorm(200)/10 + exp(1))
+kable(head(dat))
+
+dat$d <- abs(dat$x-0)
+dat_knn <- arrange(dat, dat$d)
+dat_knn
+dat_knn_final <- dat_knn[1:5,]
+dat_knn_final
+
+y_predict <- mean(dat_knn_final$d)
+y_predict
